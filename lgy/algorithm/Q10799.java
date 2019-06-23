a80a3ea4ba256f0978dc989ade6e2d6e0c916665
Einere <kjwsx23@gmail.com>
2019-06-23 16:41:54 +0900

    Initialize repository

---
+++ lgy/algorithm/Q10799.java
@@ -0,0 +1,19 @@
+package com.company.boostcamp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10799 {
+    void main() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String instruction = br.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
